da44205ca3523fee99f3ff3bddfa7a06da6ec254
Silther <info@silther.de>
2026-04-15 19:18:41 +0200

    rename plugin

--- _meta.lua
+++ _meta.lua
@@ -1,8 +1,8 @@
 
 
 return {
-    name        = "simpleui",
-    fullname    = "Simple UI",
+    name        = "simpleui_bf",
+    fullname    = "Simple UI | BookFusion",
     description = [[A simple UI for KOReader]],
     version     = "1.3.5.1",
     author      = "Doctor Hetfield",
