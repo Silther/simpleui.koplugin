755130a9eda9dad997de1fc363fba705847e5bba
Silther <info@silther.de>
2026-04-16 17:42:11 +0200

    Update _meta.lua

--- _meta.lua
+++ _meta.lua
@@ -4,6 +4,6 @@ return {
     name        = "simpleui_bf",
     fullname    = "Simple UI | BookFusion",
     description = [[A simple UI for KOReader]],
-    version     = "1.3.5.1",
+    version     = "1.3.5.2",
     author      = "Doctor Hetfield",
 }
